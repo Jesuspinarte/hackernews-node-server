629c9d8c99eccdef7fcf9364c7631f4e4aaf1161
Jesus Pineda <jesus.pineda@rappi.com>
2020-12-17 09:18:36 -0500

    [PART-001] - Simple queries and mutations

--- src/index.js
+++ src/index.js
@@ -1,22 +1,56 @@
-const { ApolloServer } = require("apollo-server");
-
-// 1
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`;
+const { ApolloServer } = require('apollo-server');
+const fs = require('fs');
+const path = require('path');
 
 // 2
+let links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL',
+  },
+];
+
+let idCount = links.length;
+
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+    link: (parent, args) => links.filter(link => link.id === args.id)[0],
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (parent, args) => {
+      const link = links.filter(link => link.id === args.id)[0];
+      if (link) {
+        args.description && (link.description = args.description);
+        args.url && (link.url = args.url);
+      }
+      return link;
+    },
+    deleteLink: (parent, args) => {
+      const link = links.filter(link => link.id === args.id)[0];
+      links.splice(link, 1);
+      return link;
+    },
   },
 };
 
 // 3
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'utils/schema.graphql'),
+    'utf8'
+  ),
   resolvers,
 });
 
